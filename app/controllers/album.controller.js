e87f93b890fc1a5752b9fe59f49b5bc1205f5b60
Sanjay <Sanjay.thipiri@eagles.oc.edu>
2022-05-30 11:39:20 -0500

    Removed publishAll method as it is not relevant to this project

--- app/controllers/album.controller.js
+++ app/controllers/album.controller.js
@@ -124,16 +124,3 @@ exports.deleteAll = (req, res) => {
       });
     });
 };
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
-  Album.findAll({ where: { published: true } })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials."
-      });
-    });
-};
